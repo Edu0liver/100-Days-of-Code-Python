4a09b9eb8f9cc6065478d5a9d1fa3315634653c9
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2022-09-19 15:43:00 -0300

    Day 16

---
+++ Day16/main.py
@@ -0,0 +1,34 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    FLAVOR_ORDER = input("\nWhat would you like? (espresso/latte/cappuccino): ")
+
+    if FLAVOR_ORDER == 'off':
+        is_on = False
+        break
+
+    if FLAVOR_ORDER == 'report':
+        print(coffee_maker.report())
+        print(money_machine.report())
+        continue
+    
+    itemInfo = menu.find_drink(FLAVOR_ORDER)
+
+    is_sufficient = coffee_maker.is_resource_sufficient(itemInfo)
+
+    if not is_sufficient:
+        break
+    
+    print("\nIt will cost you $", itemInfo.cost)
+    
+    enoughMoney = money_machine.make_payment(itemInfo.cost)
+
+    coffee_maker.make_coffee(itemInfo)
