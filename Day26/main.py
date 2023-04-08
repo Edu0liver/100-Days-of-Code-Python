61af43b12fbb469872edace3d8d7e053aff34662
Eduardo <eduardooliveira256@gmail.com>
2023-04-08 17:42:44 -0300

    Day 26: list comprehension

--- Day26/main.py
+++ Day26/main.py
@@ -1,5 +1,18 @@
+import random
 
-numbers = [1,2,3]
+numbers = [1, 2, 3]
 
 new_list = [n + 1 for n in numbers]
 print(new_list)
+
+#######################################
+
+names = ["John", "Bob", "Mosh", "Sarah", "Mary"]
+
+students_score = { student:random.randint(1, 100) for student in names }
+
+print(students_score)
+
+passed_students = { student:score for (student, score) in students_score.items() if score > 70 }
+
+print(passed_students)
